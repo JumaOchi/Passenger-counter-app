9fd9f12645d0838cae662b107abd5e13401f06b1
JumaOchi <ochiengjuma95@gmail.com>
2022-01-07 15:34:06 +0300

    Initial commit of passenger app

---
+++ index.js
@@ -0,0 +1,50 @@
+// document.getElementById("count-el").innerText = 5
+
+// initialize the count as 0
+// listen for clicks on the increment button
+// increament the count variable when the button is clicked
+// change the count-el in HTML to reflect the new count
+
+//CamelCase this is a js naming convention where we capitalize he 1st leer of the second word
+let countEL = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+   count += 1
+   countEL.innerText = count
+   console.log(count)
+}
+
+// 1. Create a function, save(),which logs out the count when its called
+
+function save() {
+    console.log(count)
+    //2. Create a variable that contains both the count and the dash seperator, i.e "12 -"
+    //3. Render the variable in the saveEl using innerText
+    //NB: Make sure to not delete the existing contents of the paragraph
+    let save = " " + count + " -"
+    saveEl.innerText += save // alternatively you can use saveEl.textcontent that will be able to read the hidden elements also
+    countEL.textContent = 0
+    count = 0
+}
+
+let username = "Jay"
+//Create a variable, message, that stores the string: "You have 3 new notifications"
+
+let message = "You have 3 new notifications"
+
+console.log(message + "," + username + "!")
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)
+
+
+
+
+
+
+
+
+
